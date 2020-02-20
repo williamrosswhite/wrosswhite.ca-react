8c242a2fe22bd33f9a888945f1e7fb6c3f815e70
rwhite83 <rwhite83@hotmail.com>
2020-02-19 19:33:15 -0800

    completed 'extras'

--- src/Components/Developer/Extras.js
+++ src/Components/Developer/Extras.js
@@ -1,43 +1,20 @@
 import React from "react";
-import text from '../../text/strings.json'
+import text from "../../text/strings.json"
+import ExtraContent from "./SubComponents/ExtraContent.js";
+import extraData from '../../text/extra-text'
 
 class Extras extends React.Component {
 
     render() {
+
+        const ex = extraData.map(item => <ExtraContent key={item.id} data={item} />)
+        
         return (
             <div className="container-fluid p-0">
-
                 <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="extras">
                     <div className="w-100">
-                        <h2 className="mb-5 spacer text-primary">{text.headers.extras}</h2>
-                        <ul className="fa-ul mb-0">
-                            <li>
-                                <i className="fa-li fa fa-trophy text-warning"></i>
-                                Google Analytics Certified Developer</li>
-                            <li>
-                                <i className="fa-li fa fa-trophy text-warning"></i>
-                                Mobile Web Specialist - Google Certification</li>
-                            <li>
-                                <i className="fa-li fa fa-trophy text-warning"></i>
-                                1<sup>st</sup>
-                                Place - University of Colorado Boulder - Emerging Tech Competition 2009</li>
-                            <li>
-                                <i className="fa-li fa fa-trophy text-warning"></i>
-                                1<sup>st</sup>
-                                Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)</li>
-                            <li>
-                                <i className="fa-li fa fa-trophy text-warning"></i>
-                                2<sup>nd</sup>
-                                Place - University of Colorado Boulder - Emerging Tech Competition 2008</li>
-                            <li>
-                                <i className="fa-li fa fa-trophy text-warning"></i>
-                                1<sup>st</sup>
-                                Place - James Buchanan High School - Hackathon 2006</li>
-                            <li>
-                                <i className="fa-li fa fa-trophy text-warning"></i>
-                                3<sup>rd</sup>
-                                Place - James Buchanan High School - Hackathon 2005</li>
-                        </ul>
+                        <h2 className="mb-0 spacer text-primary">{text.headers.extras}</h2>
+                        {ex}
                     </div>
                 </section>
             </div>
--- src/Components/Developer/Interests.js
+++ src/Components/Developer/Interests.js
@@ -5,7 +5,6 @@ class Interests extends React.Component {
     render() {
         return (
             <div className="container-fluid p-0">
-
                 <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
                     <div className="w-100">
                         <h2 className="mb-5 spacer text-primary">Interests</h2>
