cbe6409dbab5d56b463c0660b307f55c3555549c
W. Ross White <rwhite83@hotmail.com>
2024-09-07 13:23:49 -0700

    update sept 2024

--- src/text/experience-text.js
+++ src/text/experience-text.js
@@ -5,13 +5,28 @@ const experienceData = [
         "name": "AlayaCare",
         "title": "Associate Developer",
         "sector": "Home Care Software | Home Health Software",
-        "date": "June 2020 - Present",
+        "date": "June 2020 - April 2023",
         "bullets": {
             "zero": "Leading development on a new Fee Capping finance module being added to the AlayaCare residential care home management platform.",
             "one": "Spend my days working in a .NET environment using Visual Studio with C# backend and AngularJS front end.",
             "two": "Have been delving into pipeline management and deployment.  The new feature I've been developing leverages a docker based billing microservice.  I have had to learn and work with Docker, Kubernetes, RabbitMQ workers, and automated API based communications between our monolith project and our newer microservices.",
             "three": "Full stack developer: regularly work up and down the stack from manipulating database tables through to finicky UI CSS work.  Experienced in Entity Framework, C#, Visual Studio, Integration/Unit Tests, OOP design, Docker, Typescript, AngularJS, and front end interpolation.",
             "four": "Deal a lot with very complicated financial data structures, including double entry finance journals."
+        }
+    },
+    {
+        id: 1,
+        "code": "quartech",
+        "name": "Quartech",
+        "title": "Full-Time Software Developer",
+        "sector": "Contracted Software Development",
+        "date": "April 2023 - Present",
+        "bullets": {
+            "zero": "Full Stack Developer working primarily on a rehabilitation and reintegration case management system for Partners in Canadian Veterans Rehabilitation Services on behalf of Veterans Affairs Canada.",
+            "one": "Included a separate front end portal into the same backend.",
+            "two": "System largely operated through workflows using Formly, in addition to other development up and down the stack using C#, Typescript, EntityFramework/SQL Studio, and other modern tools.",
+            "three": "Full stack developer: regularly work up and down the stack from manipulating database tables through to finicky UI CSS work.  Experienced in Entity Framework, C#, Visual Studio, Integration/Unit Tests, OOP design, Docker, Typescript, AngularJS, and front end interpolation.",
+            "four": "Worked with a BC emergency response platform for people facing evacuation, as well as a Cannabis retailer verification platform for LCRB."
         }
     },
 ]
